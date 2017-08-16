dc0bfd732f4f1881e270c0710d484cff60576664
ng-ninja <varunkrishnareddy@gmail.com>
2017-08-16 16:29:20 -0700

    our first heroku commit

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react'
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h2>
-                    Home!
-                </h2>
-            </div>
-        )
-    }
+
+  render() {
+    return (
+      <div>
+        <h2>
+            Home!
+        </h2>
+      </div>
+    )
+  }
 }
 
 export default Home
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import {Route, IndexRoute} from 'react-router'
 import Template from '../containers/Template'
+import Home from '../containers/Home'
+import Profile from '../containers/Profile'
 
 const createRoutes = () => {
   return (
-    <Route
-      path='/'
-      component={Template}
-    >
+      <Route
+          path='/'
+          component={Template}>
+        <IndexRoute
+            component={Home}/>
+        <Route
+            path={'/profile'}
+            component={Profile}
+        />
 
-    </Route>
+
+      </Route>
   )
 }
 
